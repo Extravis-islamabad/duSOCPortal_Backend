7c6e7ceea87f4f3e316b04deae42e4ca37f6e271
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-08 18:47:28 +0500

    Added more fields to user model

--- authentication/models.py
+++ authentication/models.py
@@ -5,6 +5,10 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    is_super_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
