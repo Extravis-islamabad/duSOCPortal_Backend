5ad6b638c7430a013d45b4b5510e568b94d51238
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-08 18:39:36 +0500

    Created authentication app and added User model

---
+++ authentication/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+# class OrderStatus(models.IntegerChoices):
+#     Awaiting_Pickup = 1
+#     Picked_up = 2
+#     At_QWQER_Warehouse = 3
+#     On_Route_to_Delivery = 4
+#     Delivered = 5
+#     Attempted = 6
+#     Reattempt_for_Delivery = 7
+#     Cancelled = 8
