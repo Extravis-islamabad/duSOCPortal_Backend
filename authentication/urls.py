51700a6f6fee3974c251d2b57b8271b00efd3468
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-10 10:56:50 +0500

    refresh token endpoint added

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import UserCreateAPIView, UserLoginAPIView
 
 urlpatterns = [
     path("create_user/", UserCreateAPIView.as_view(), name="user-create"),
     path("login/", UserLoginAPIView.as_view(), name="user-login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
