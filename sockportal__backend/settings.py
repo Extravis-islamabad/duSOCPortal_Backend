ae288f81c5ce3723986486f514b10463bcd5ba6f
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-11 15:52:34 +0500

    added cors headers

--- sockportal__backend/settings.py
+++ sockportal__backend/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "rest_framework_simplejwt",
 ]
 
 INSTALLED_APPS += ["authentication", "tenant", "tenantadmin"]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -58,6 +60,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 ROOT_URLCONF = "sockportal__backend.urls"
 
 TEMPLATES = [
