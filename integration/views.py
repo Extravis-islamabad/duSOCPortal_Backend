43efec3fb2b9fb0b294f8be650d63adb57b54ee8
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-22 09:57:55 +0500

    updated the view to return the subtypes data as well

--- integration/views.py
+++ integration/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from .models import IntegrationTypes
+from .models import IntegrationTypes, ItsmSubTypes, SiemSubTypes, SoarSubTypes
 
 
 class IntegrationTypesView(APIView):
@@ -12,7 +12,28 @@ class IntegrationTypesView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        siem_subtypes = [
+            {"id": choice[0], "name": choice[1]} for choice in SiemSubTypes.choices
+        ]
+        soar_subtypes = [
+            {"id": choice[0], "name": choice[1]} for choice in SoarSubTypes.choices
+        ]
+        itsm_subtypes = [
+            {"id": choice[0], "name": choice[1]} for choice in ItsmSubTypes.choices
+        ]
+
         integration_types = [
-            {"id": choice[0], "name": choice[1]} for choice in IntegrationTypes.choices
+            {
+                "id": choice[0],
+                "name": choice[1],
+                "sub_types": (
+                    siem_subtypes
+                    if choice[0] == IntegrationTypes.SIEM_INTEGRATION
+                    else soar_subtypes
+                    if choice[0] == IntegrationTypes.SOAR_INTEGRATION
+                    else itsm_subtypes
+                ),
+            }
+            for choice in IntegrationTypes.choices
         ]
         return Response({"data": integration_types}, status=status.HTTP_200_OK)
