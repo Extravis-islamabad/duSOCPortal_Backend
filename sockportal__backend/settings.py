e075ea3f4a55363ccd6e54536baac070be122a57
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-30 16:03:38 +0500

    added the static root

--- sockportal__backend/settings.py
+++ sockportal__backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -148,6 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 
 # Default primary key field type
