cee2dbea679616a603359037567577acc1b52681
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-04-30 15:57:28 +0500

    adding the script

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Starting Django development server on port ${PORT:-8000}..."
-python manage.py runserver 0.0.0.0:${PORT:-8000}
+echo "Starting Django app with Uvicorn on port ${PORT:-8000}..."
+uvicorn sockportal__backend.asgi:application --host 0.0.0.0 --port ${PORT:-8000} --workers 1
