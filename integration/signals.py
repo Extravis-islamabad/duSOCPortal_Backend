ce319beae8c025b6981387d3ea7d4636b5c5766d
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-05-12 17:14:49 +0500

    added the signals

---
+++ integration/signals.py
@@ -0,0 +1,39 @@
+# integrations/signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from tenant.ibm_qradar_tasks import sync_event_collectors, sync_qradar_tenants
+
+from .models import (
+    CredentialTypes,
+    IntegrationCredentials,
+    IntegrationTypes,
+    SiemSubTypes,
+)
+
+
+@receiver(post_save, sender=IntegrationCredentials)
+def trigger_integration_tasks(
+    sender, instance: IntegrationCredentials, created, **kwargs
+):
+    if created:
+        if instance.integration.integration_type == IntegrationTypes.SIEM_INTEGRATION:
+            if instance.integration.siem_subtype == SiemSubTypes.IBM_QRADAR:
+                if instance.credential_type == CredentialTypes.USERNAME_PASSWORD:
+                    username = instance.username
+                    password = getattr(
+                        instance, "_plaintext_password", None
+                    )  # Get plaintext password
+                    ip_address = instance.ip_address
+                    port = instance.port
+
+                    kwargs = {
+                        "username": username,
+                        "password": password,
+                        "ip_address": ip_address,
+                        "port": port,
+                        "integration_id": instance.integration.id,
+                    }
+                    sync_qradar_tenants.delay(**kwargs)
+                    sync_event_collectors.delay(**kwargs)
