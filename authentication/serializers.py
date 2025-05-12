7483517115503fe8820ea34ae2da2cf36e28962a
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-05-12 12:50:10 +0500

    updated the user details endpoint to return the permissions

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from tenant.models import Tenant, TenantRole, TenantRolePermissions
+
 from .models import User
 
 
@@ -20,6 +22,8 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
+    permissions = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
@@ -31,5 +35,35 @@ class UserDetailSerializer(serializers.ModelSerializer):
             "is_admin",
             "created_at",
             "updated_at",
+            "permissions",
         ]
         read_only_fields = ["id", "created_at", "updated_at"]
+
+    def get_permissions(self, obj):
+        """
+        Returns the list of permissions for tenants or an empty list for admins.
+        """
+        if obj.is_tenant:
+            try:
+                # Fetch the tenant profile associated with the user
+                tenant = Tenant.objects.get(tenant=obj)
+                # Fetch the tenant's role
+                tenant_role = TenantRole.objects.filter(tenant=tenant).first()
+                if tenant_role:
+                    # Fetch all permissions associated with the role
+                    role_permissions = TenantRolePermissions.objects.filter(
+                        role=tenant_role
+                    )
+                    # Return permissions as a list of dictionaries
+                    return [
+                        {
+                            "id": perm.permission,
+                            "name": perm.permission_text,
+                        }
+                        for perm in role_permissions
+                    ]
+            except Tenant.DoesNotExist:
+                # If no tenant profile exists, return an empty list
+                return []
+        # For admins (or non-tenants), return an empty list
+        return []
