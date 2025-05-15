24d448df08ef66d43ba50e17bbfc99c4d409caa7
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-05-15 12:56:05 +0500

    added celery tasks for cortex soar

---
+++ tenant/cortex_soar_tasks.py
@@ -0,0 +1,90 @@
+import time
+
+from celery import shared_task
+from loguru import logger
+
+from common.modules.cortex_soar import CortexSOAR
+
+
+@shared_task
+def sync_cortex_soar_tenants(
+    token: str, ip_address: str, port: int, integration_id: int
+):
+    """
+    Syncs the CortexSOAR tenants with the database.
+
+    This task fetches the accounts from the CortexSOAR instance and
+    inserts them into the database.
+
+    :param token: The authentication token to use when logging into the CortexSOAR.
+    :param ip_address: The IP address of the CortexSOAR instance.
+    :param port: The port to use when connecting to the CortexSOAR instance.
+    :param integration_id: The ID of the integration for which to sync the tenants.
+    """
+    start = time.time()
+    try:
+        with CortexSOAR(ip_address=ip_address, port=port, token=token) as soar:
+            accounts = soar._get_accounts()
+            if accounts is None:
+                logger.error("No data returned from CortexSOAR accounts endpoint")
+                return
+
+            transformed_data = soar._transform_accounts(
+                accounts=accounts, integration_id=integration_id
+            )
+            if not isinstance(transformed_data, list):
+                logger.error("Invalid data format: Expected a list")
+                return
+
+            soar._insert_accounts(transformed_data)
+
+            logger.info(
+                f"Successfully synced {len(transformed_data)} CortexSOAR tenants"
+            )
+            logger.info(
+                f"CortexSOAR.sync_itsm_tenants() task took {time.time() - start} seconds"
+            )
+    except Exception as e:
+        logger.error(f"Unexpected error in sync_itsm_tenants: {str(e)}")
+
+
+@shared_task
+def sync_cortex_soar_tenants_test(
+    token: str, ip_address: str, port: int, integration_id: int
+):
+    """
+    Syncs the CortexSOAR tenants with the database.
+
+    This task fetches the accounts from the CortexSOAR instance and
+    inserts them into the database.
+
+    :param token: The authentication token to use when logging into the CortexSOAR.
+    :param ip_address: The IP address of the CortexSOAR instance.
+    :param port: The port to use when connecting to the CortexSOAR instance.
+    :param integration_id: The ID of the integration for which to sync the tenants.
+    """
+    start = time.time()
+    try:
+        with CortexSOAR(ip_address=ip_address, port=port, token=token) as soar:
+            accounts = soar._get_accounts_test()
+            if accounts is None:
+                logger.error("No data returned from CortexSOAR accounts endpoint")
+                return
+
+            transformed_data = soar._transform_accounts(
+                accounts=accounts, integration_id=integration_id
+            )
+            if not isinstance(transformed_data, list):
+                logger.error("Invalid data format: Expected a list")
+                return
+
+            soar._insert_accounts(transformed_data)
+
+            logger.info(
+                f"Successfully synced {len(transformed_data)} CortexSOAR tenants"
+            )
+            logger.info(
+                f"CortexSOAR.sync_itsm_tenants() task took {time.time() - start} seconds"
+            )
+    except Exception as e:
+        logger.error(f"Unexpected error in sync_itsm_tenants: {str(e)}")
