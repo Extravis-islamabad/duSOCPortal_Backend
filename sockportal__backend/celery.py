268248a90572b8fa761bd78965352a7449ad5a2c
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-05-27 10:47:47 +0500

    cron tab added

--- sockportal__backend/celery.py
+++ sockportal__backend/celery.py
@@ -1,9 +1,26 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sockportal__backend.settings")
 
 app = Celery("sockportal__backend")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    # "itsm-sync-tasks": {
+    #     "task": "tenant.itsm_tasks.sync_itsm",
+    #     "schedule": crontab(minute="*/30"),
+    # },
+    # "cortex-sync-tasks": {
+    #     "task": "tenant.cortex_soar_tasks.sync_soar_data",
+    #     "schedule": crontab(minute="*/35"),
+    # },
+    "qradar-sync-tasks": {
+        "task": "tenant.ibm_qradar_tasks.sync_ibm_qradar_data",
+        "schedule": crontab(minute="*/2"),
+    },
+}
