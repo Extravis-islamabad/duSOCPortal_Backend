4e4025ef1dba00f6f1aa5a2659912f9eb243f8b2
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-05-27 11:22:10 +0500

    added final cron tabs

--- sockportal__backend/celery.py
+++ sockportal__backend/celery.py
@@ -11,16 +11,16 @@ app.autodiscover_tasks()
 
 
 app.conf.beat_schedule = {
+    "qradar-sync-tasks": {
+        "task": "tenant.ibm_qradar_tasks.sync_ibm_qradar_data",
+        "schedule": crontab(minute="*/30"),
+    },
     "itsm-sync-tasks": {
         "task": "tenant.itsm_tasks.sync_itsm",
-        "schedule": crontab(minute="*/2"),
+        "schedule": crontab(minute="*/35"),
     },
-    # "cortex-sync-tasks": {
-    #     "task": "tenant.cortex_soar_tasks.sync_soar_data",
-    #     "schedule": crontab(minute="*/2"),
-    # },
-    # "qradar-sync-tasks": {
-    #     "task": "tenant.ibm_qradar_tasks.sync_ibm_qradar_data",
-    #     "schedule": crontab(minute="*/2"),
-    # },
+    "cortex-sync-tasks": {
+        "task": "tenant.cortex_soar_tasks.sync_soar_data",
+        "schedule": crontab(minute="*/40"),
+    },
 }
