685167f00fc2ba18d06cac76af5bb5473ab58a26
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-05-27 10:43:41 +0500

    added

--- integration/signals.py
+++ integration/signals.py
@@ -3,9 +3,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from tenant.cortex_soar_tasks import sync_cortex_soar_tenants
-from tenant.ibm_qradar_tasks import sync_event_collectors, sync_qradar_tenants
-from tenant.itsm_tasks import sync_itsm_tenants
+from tenant.cortex_soar_tasks import sync_cortex_soar_tenants, sync_soar_data
+from tenant.ibm_qradar_tasks import (
+    sync_event_collectors,
+    sync_event_log_assets,
+    sync_ibm_qradar_data,
+    sync_qradar_tenants,
+)
+from tenant.itsm_tasks import sync_itsm, sync_itsm_tenants
 
 from .models import (
     CredentialTypes,
@@ -44,6 +49,8 @@ def trigger_integration_tasks(
                     }
                     sync_qradar_tenants.delay(**kwargs)
                     sync_event_collectors.delay(**kwargs)
+                    sync_event_log_assets.delay(**kwargs)
+                    sync_ibm_qradar_data.delay()
 
         elif instance.integration.integration_type == IntegrationTypes.ITSM_INTEGRATION:
             if instance.integration.itsm_subtype == ItsmSubTypes.MANAGE_ENGINE:
@@ -58,6 +65,7 @@ def trigger_integration_tasks(
                         "integration_id": instance.integration.id,
                     }
                     sync_itsm_tenants.delay(**kwargs)
+                    sync_itsm.delay()
 
         elif instance.integration.integration_type == IntegrationTypes.SOAR_INTEGRATION:
             if instance.integration.soar_subtype == SoarSubTypes.CORTEX_SOAR:
@@ -73,3 +81,4 @@ def trigger_integration_tasks(
                     }
 
                     sync_cortex_soar_tenants.delay(**kwargs)
+                    sync_soar_data.delay()
