ebcdc679139a025bf8e25d3d2425f377190258f0
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-06-02 15:45:28 +0500

    added the background tasks

---
+++ tenant/threat_intelligence.py
@@ -0,0 +1,38 @@
+from celery import shared_task
+
+from common.modules.cyware import Cyware
+from integration.models import (
+    CredentialTypes,
+    IntegrationCredentials,
+    IntegrationTypes,
+    ThreatIntelligenceSubTypes,
+)
+
+
+@shared_task
+def sync_threat_intel():
+    results = IntegrationCredentials.objects.filter(
+        integration__integration_type=IntegrationTypes.THREAT_INTELLIGENCE,
+        integration__threat_intelligence_subtype=ThreatIntelligenceSubTypes.CYWARE,
+        credential_type=CredentialTypes.SECRET_KEY_ACCESS_KEY,
+    )
+
+    for result in results:
+        with Cyware(
+            access_key=result.access_key,
+            secret_key=result.secret_key,
+            base_url=result.base_url,
+        ) as cyware:
+            all_alerts = cyware.fetch_all_alerts(page_size=1000)
+            transformed_data = cyware.transform_alert(all_alerts, result.integration.id)
+            cyware.insert_alerts(transformed_data)
+
+
+@shared_task
+def sync_threat_intel_with_signal(access_key, secret_key, base_url, integration_id):
+    with Cyware(
+        access_key=access_key, secret_key=secret_key, base_url=base_url
+    ) as cyware:
+        all_alerts = cyware.fetch_all_alerts(page_size=1000)
+        transformed_data = cyware.transform_alert(all_alerts, integration_id)
+        cyware.insert_alerts(transformed_data)
