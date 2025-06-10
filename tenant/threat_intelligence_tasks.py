6f7a390499cff2d874f1ca9123e076e8516ca390
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-06-10 12:16:34 +0500

    added the tasks for dumping the threat intel tags

--- tenant/threat_intelligence_tasks.py
+++ tenant/threat_intelligence_tasks.py
@@ -40,6 +40,27 @@ def sync_threat_intel_with_signal(access_key, secret_key, base_url, integration_
 
 
 @shared_task
+def sync_threat_tags():
+    results = IntegrationCredentials.objects.filter(
+        integration__integration_type=IntegrationTypes.THREAT_INTELLIGENCE,
+        integration__threat_intelligence_subtype=ThreatIntelligenceSubTypes.CYWARE,
+        credential_type=CredentialTypes.SECRET_KEY_ACCESS_KEY,
+    )
+
+    for result in results:
+        with Cyware(
+            access_key=result.access_key,
+            secret_key=result.secret_key,
+            base_url=result.base_url,
+        ) as cyware:
+            tags = cyware.get_list_tags()
+            transformed_data = cyware.transform_tags(
+                data=tags, integration=result.integration.id
+            )
+            cyware.insert_tags(tags=transformed_data)
+
+
+@shared_task
 def sync_threat_intel_for_tenants():
     results = ThreatIntelligenceTenant.objects.all()
 
