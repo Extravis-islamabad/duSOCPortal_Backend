7e8343e657c655a5252a3e3ba72a9f1784090fd6
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-06-13 12:44:04 +0500

    changes in model and ran the migrations

--- authentication/models.py
+++ authentication/models.py
@@ -47,6 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     company_name = models.CharField(max_length=100, blank=True, null=True)
     is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
