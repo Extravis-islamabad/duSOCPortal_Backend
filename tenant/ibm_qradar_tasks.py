dcd71a7df4e91b0cccc5901a010a8f9f071bb8d7
Talha Khan <talha.khan@nets-international.com>
2025-07-10 20:28:49 +0500

    test

--- tenant/ibm_qradar_tasks.py
+++ tenant/ibm_qradar_tasks.py
@@ -986,7 +986,7 @@ def sync_suspicious_for_admin(username, password, ip_address, port, integration_
 
     db_ids = DuIbmQradarTenants.objects.values_list("db_id", flat=True)
 
-    today = datetime.today().date() - timedelta(days=3)
+    today = datetime.today().date() - timedelta(days=4)
     # Get today's date range
     # TODO : Commenting this
     # today = datetime.today().date()
