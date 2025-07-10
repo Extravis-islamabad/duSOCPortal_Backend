135a01f17a383470ed105e341039ddeb9dad0f7f
Talha Khan <talha.khan@nets-international.com>
2025-07-10 22:53:34 +0500

    test

--- tenant/ibm_qradar_tasks.py
+++ tenant/ibm_qradar_tasks.py
@@ -1624,7 +1624,7 @@ def sync_monthly_avg_eps_for_admin(
 
     db_ids = DuIbmQradarTenants.objects.values_list("db_id", flat=True)
 
-    today = datetime.today().date() - timedelta(days=3)
+    today = datetime.today().date() - timedelta(days=4)
     # Get today's date range
     # TODO: Commenting this
     # today = datetime.today().date()
