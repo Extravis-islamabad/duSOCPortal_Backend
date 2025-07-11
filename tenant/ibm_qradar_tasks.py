11c7e482bc41021f639d9b33b4d79f82c3b644c4
Talha Khan <talha.khan@nets-international.com>
2025-07-11 13:14:03 +0500

    test

--- tenant/ibm_qradar_tasks.py
+++ tenant/ibm_qradar_tasks.py
@@ -474,7 +474,7 @@ def sync_event_count_for_admin(username, password, ip_address, port, integration
 
     db_ids = DuIbmQradarTenants.objects.values_list("db_id", flat=True)
 
-    today = datetime.today().date() - timedelta(days=3)
+    today = datetime.today().date() - timedelta(days=4)
     # Get today's date range
     # TODO: Commenting this
     # today = datetime.today().date()
