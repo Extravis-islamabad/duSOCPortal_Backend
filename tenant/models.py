f50eaf24682d9f6e396f50c625b0daab8ba25c09
Arehman-Nets <abdul.rehman@nets-international.com>
2025-07-30 16:12:24 +0500

    IBMQraderCorelatedEvents added change in user column migration file

--- tenant/models.py
+++ tenant/models.py
@@ -134,11 +134,12 @@ class IBMQraderAEPEntraAuthentication(models.Model):
     )
     start_time = models.DateTimeField()
     event_name = models.CharField(max_length=255, blank=True, default=None)
-    user = models.CharField(max_length=20, blank=True, default=None)
+    user = models.CharField(max_length=255, blank=True, default=None)
     low_level_category = models.CharField(max_length=255, blank=True, default=None)
     total_event_count = models.CharField(max_length=20, blank=True, default=None)
     count = models.CharField(max_length=20, blank=True, default=None)
-    created_at = models.DateTimeField(auto_now_add=True)
+    #created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(blank=True, null=True, default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
