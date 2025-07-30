42b804449da44d7eeb759e38272efc7adc33697b
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-07-30 16:18:55 +0500

    added

--- tenant/migrations/0170_alter_ibmqraderaepentraauthentication_user.py
+++ tenant/migrations/0170_alter_ibmqraderaepentraauthentication_user.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("tenant", "0169_alter_ibmqraderaepentraauthentication_created_at"),
     ]
