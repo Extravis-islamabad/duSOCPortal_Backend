e95309d9488bab5a4bfd40ebf5dc8bf293d54c9d
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-09-09 09:59:19 +0500

    updated for the itsm

--- tenant/views.py
+++ tenant/views.py
@@ -238,9 +238,25 @@ class DuITSMTenantsListView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request):
-        tenants = DuITSMTenants.objects.all()
-        serializer = DuITSMTenantsSerializer(tenants, many=True)
-        return Response(serializer.data)
+        # Check if integration_id is provided
+        integration_id = request.query_params.get("integration_id", None)
+
+        if not integration_id:
+            return Response(
+                {"error": "Pass a valid integration_id"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            # Filter tenants by integration_id
+            tenants = DuITSMTenants.objects.filter(integration_id=integration_id)
+            serializer = DuITSMTenantsSerializer(tenants, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(
+                {"error": f"An error occurred: {str(e)}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
 
 # test
