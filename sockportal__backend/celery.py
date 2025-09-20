a43e9bdd081719adf7f62774bd6fef0840826803
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-09-20 12:23:46 +0500

    uncommented the cyware

--- sockportal__backend/celery.py
+++ sockportal__backend/celery.py
@@ -113,19 +113,19 @@ app.conf.beat_schedule = {
         "schedule": crontab(minute="*/5"),
         "options": {"queue": "soar"},
     },
-    # "threat-intelligence-sync-tasks": {
-    #     "task": "tenant.threat_intelligence_tasks.default_cyware",
-    #     "schedule": crontab(minute="*/5"),
-    #     "options": {"queue": "cyware"},
-    # },
-    # "threat-intelligence-tenant-sync-tasks": {
-    #     "task": "tenant.threat_intelligence_tasks.custom_cyware",
-    #     "schedule": crontab(minute="*/5"),
-    #     "options": {"queue": "cyware"},
-    # },
-    # "threat-intelligence-all-sync-tasks": {
-    #     "task": "tenant.threat_intelligence_tasks.sync_threat_intel_all",
-    #     "schedule": crontab(minute="*/30"),
-    #     "options": {"queue": "cyware"},
-    # },
+    "threat-intelligence-sync-tasks": {
+        "task": "tenant.threat_intelligence_tasks.default_cyware",
+        "schedule": crontab(minute="*/5"),
+        "options": {"queue": "cyware"},
+    },
+    "threat-intelligence-tenant-sync-tasks": {
+        "task": "tenant.threat_intelligence_tasks.custom_cyware",
+        "schedule": crontab(minute="*/5"),
+        "options": {"queue": "cyware"},
+    },
+    "threat-intelligence-all-sync-tasks": {
+        "task": "tenant.threat_intelligence_tasks.sync_threat_intel_all",
+        "schedule": crontab(minute="*/30"),
+        "options": {"queue": "cyware"},
+    },
 }
