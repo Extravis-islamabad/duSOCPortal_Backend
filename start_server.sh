799571c36641c4084fd39e253470319b0a32088c
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2025-12-05 11:20:30 +0500

    removed the twice collection of the static files

--- start_server.sh
+++ start_server.sh
@@ -6,8 +6,5 @@ python manage.py collectstatic --noinput
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Collecting the Static files"
-python manage.py collectstatic --noinput
-
 echo "Starting Django app with Uvicorn on port ${PORT:-8000}..."
 uvicorn sockportal__backend.asgi:application --host 0.0.0.0 --port ${PORT:-8000} --workers 1
