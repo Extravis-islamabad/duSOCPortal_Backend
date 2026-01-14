36c0ed2a194e5703fe6722053643213740bb2542
Mutahhar-Nets <Mutahhar.Abbas@nets-international.com>
2026-01-14 13:40:12 +0500

    Feat: Wrote the celery tasks for the fortisoar tenant sync Wrote the transformation functions and the inertion functions for the tenant sync of forti soar

--- common/modules/fortisoar.py
+++ common/modules/fortisoar.py
@@ -1,9 +1,12 @@
 import time
 
+import pandas as pd
 import requests
+from django.db import transaction
 from loguru import logger
 
 from common.constants import EnvConstants, FortiSOARConstants, SSLConstants
+from tenant.models import FortiSOARTenants
 
 
 class FortiSOAR:
@@ -104,6 +107,20 @@ class FortiSOAR:
         return data
 
     def _get_alerts(self, tenant_name: str, timeout=SSLConstants.TIMEOUT):
+        """
+        Fetches the list of alerts from the FortiSOAR endpoint.
+
+        This method sends an HTTP GET request to the FortiSOAR alerts API endpoint
+        to retrieve the list of alerts for a given tenant. It uses HTTP basic authentication
+        with the credentials set in the FortiSOARConstants. If the request is successful, it returns
+        the parsed JSON response. If the request fails or an exception occurs, it logs an error
+        and returns an empty dictionary.
+
+        :param tenant_name: The name of the tenant.
+        :param timeout: The timeout for the request in seconds.
+        :return: A dictionary containing alert information if the request is successful, otherwise an empty dictionary.
+        :raises: Logs any exceptions that occur during the request.
+        """
         start = time.time()
         logger.info(f"FortiSOAR._get_alerts() started : {start}")
         endpoint = f"{self.base_url}/{FortiSOARConstants.ALERTS_ENDPOINT}?tenant__name={tenant_name}"
@@ -141,40 +158,49 @@ class FortiSOAR:
         data = response.json()
         return data
 
-    def _get_notes(self, tenant_name: str, timeout=SSLConstants.TIMEOUT):
+    def transform_tenants(self, data, integration_id):
+        """
+        Transforms the list of tenants from the FortiSOAR API response into a list of dictionaries.
+
+        :param data: A dictionary containing the FortiSOAR API response.
+        :param integration_id: The integration ID to be associated with the tenants.
+        :return: A list of dictionaries containing the transformed tenant information.
+        """
+        tenants_data = data.get("hydra:member", [])
+
+        if not tenants_data:
+            logger.warning("No tenants found in FortiSOAR API response")
+            return []
+
+        df = pd.DataFrame(tenants_data)
+        df = df[["id", "name"]]
+        df.rename(columns={"id": "db_id"}, inplace=True)
+        df["integration_id"] = integration_id
+        results = df.to_dict(orient="records")
+        return results
+
+    def _insert_tenants(self, accounts: dict):
+        """
+        Inserts or updates tenant records in the FortiSOARTenants table.
+
+        :param accounts: A list of dictionaries containing tenant information.
+        """
         start = time.time()
-        logger.info(f"FortiSOAR._get_alerts() started : {start}")
-        endpoint = f"{self.base_url}/{FortiSOARConstants.ALERTS_ENDPOINT}?tenant__name={tenant_name}"
+        logger.info(f"FortiSOAR._insert_accounts() started : {start}")
+        records = [FortiSOARTenants(**item) for item in accounts]
+        logger.info(f"Inserting the accounts records: {len(records)}")
         try:
-            if EnvConstants.LOCAL:
-                proxies = {
-                    "http": "http://127.0.0.1:8080",
-                    "https": "http://127.0.0.1:8080",
-                }
-                response = requests.get(
-                    endpoint,
-                    headers=self.headers,
-                    verify=SSLConstants.VERIFY,
-                    proxies=proxies,
-                    timeout=timeout,
+            with transaction.atomic():
+                FortiSOARTenants.objects.bulk_create(
+                    records,
+                    update_conflicts=True,
+                    update_fields=["name"],
+                    unique_fields=["db_id"],
                 )
-            else:
-                response = requests.get(
-                    endpoint,
-                    headers=self.headers,
-                    verify=SSLConstants.VERIFY,
-                    timeout=timeout,
+                logger.info(f"Inserted the accounts records: {len(records)}")
+                logger.success(
+                    f"FortiSOAR._insert_accounts() took: {time.time() - start} seconds"
                 )
         except Exception as e:
-            logger.error(f"FortiSOAR._get_alerts() failed with exception : {str(e)}")
-            raise Exception(f"FortiSOAR._get_alerts() failed with exception : {str(e)}")
-        if response.status_code != 200:
-            logger.warning(
-                f"FortiSOAR._get_alerts() return the status code {response.status_code}"
-            )
-            raise Exception(
-                f"FortiSOAR._get_alerts() return the status code {response.status_code}"
-            )
-
-        data = response.json()
-        return data
+            logger.error(f"An error occurred in FortiSOAR._insert_accounts(): {str(e)}")
+            transaction.rollback()
---
+++ tenant/forti_soar_tasks.py
@@ -0,0 +1,51 @@
+import time
+
+from celery import shared_task
+from loguru import logger
+
+from common.modules.fortisoar import FortiSOAR
+from integration.models import (
+    CredentialTypes,
+    IntegrationCredentials,
+    IntegrationTypes,
+    SoarSubTypes,
+)
+
+
+def sync_forti_soar_tenants(token, ip_address, port, integration_id):
+    logger.info("Running sync_forti_soar_tenants() task")
+    start = time.time()
+    try:
+        with FortiSOAR(ip_address, port, token) as fsoar:
+            data = fsoar._get_tenants()
+            if not data.get("hydra:member"):
+                return
+            transformed_data = fsoar.transform_tenants(data, integration_id)
+            if not transformed_data:
+                return
+
+            fsoar._insert_tenants(transformed_data)
+    except Exception as e:
+        logger.error(f"Error sync_forti_soar_tenants: {e}")
+    logger.info(f"Successfully synced {len(transformed_data)} tenants")
+    logger.info(
+        f"FortiSOARTenants.sync_forti_soar_tenants() task took {time.time() - start} seconds"
+    )
+
+
+@shared_task
+def sync_forti_soar_data():
+    logger.info("Running sync_forti_soar_data() task")
+    results = IntegrationCredentials.objects.filter(
+        integration__integration_type=IntegrationTypes.SOAR_INTEGRATION,
+        integration__soar_subtype=SoarSubTypes.FORTI_SOAR,
+        credential_type=CredentialTypes.API_KEY,
+    )
+
+    for result in results:
+        sync_forti_soar_tenants(
+            token=result.api_key,
+            ip_address=result.ip_address,
+            port=result.port,
+            integration_id=result.integration.id,
+        )
